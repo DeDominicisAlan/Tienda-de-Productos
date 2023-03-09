5bb1b813f9ca87adc292bd4db4df8cadef1d5b70
alan <dedominicisalan@gmail.com>
2023-03-09 12:20:46 -0300

    El boton de resetear ahora funciona, vaciando la lista y estableciendo las cantidades de cada producto en 0

--- main.js
+++ main.js
@@ -30,7 +30,15 @@
     reset.addEventListener('click',() =>{
         suma = 0
         total.textContent = ""
+        carrito.innerHTML = ""
+        vaciarProductos()
     })
+    
+    function vaciarProductos(){
+        items.forEach(element => {
+            element.cant = 0
+        });
+    }
     
     items.forEach(element => {
         const nodo = document.createElement('div')
@@ -69,23 +77,37 @@
         
         function crearProducto(prod){
             if(prod.cant == 0){
+            //Un contenedor para el producto
             const producto = document.createElement('li')
+            producto.classList.add ('lista-item')
+            //Creo los espacios para los datos del producto
             const productoNombre = document.createElement('p')
             const productoCant = document.createElement('p')
             const productoPrecio = document.createElement('p')
-            productoPrecio.
+            
+            //Le agregamos ID a los datos del producto para poder modificarlos despues
             productoCant.id = "Producto"+prod.id
+            productoPrecio.id = "Precio"+prod.id
+            
+            //Le agregamos los datos
             prod.cant++
             productoNombre.textContent = prod.nombre
             productoCant.textContent = prod.cant
+            productoPrecio.textContent = prod.precio
             
+            //Los agregamos en la lista y agregamos los datos del producto a su contenedor de producto
             producto.appendChild(productoCant)
             carrito.appendChild(producto)
             producto.appendChild(productoNombre)
+            producto.appendChild(productoPrecio)
             
             }else{
+                //En caso que ya exista el producto, modificamos sus datos
+                //Accedemos mediante su ID
                 const producto = document.querySelector('#Producto'+prod.id)
+                const precio = document.querySelector('#Precio'+prod.id)
                 producto.innerHTML = prod.cant
+                precio.innerHTML = parseInt(precio.innerHTML,10) + prod.precio
             }
             prod.cant++
             
